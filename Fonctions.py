14260815856e5959169e2399b8af56515f7f6eef
Lyse-May <59876881+Lyse-May@users.noreply.github.com>
2020-02-20 10:19:50 +0100

    Update Fonctions.py

--- Fonctions.py
+++ Fonctions.py
@@ -23,3 +23,27 @@ Rs = 2.90
 a1 = 0.79
 a2 = 0.01
 
+class Household:
+    
+    def Wages(self, GDP):
+        return (1-gamma)*GDP
+    
+    def Housing_Transaction(self, Delta_Ph):
+        return phi1 + phi2*(Delta_Ph)
+    
+class Government:
+    
+    def Government_Bond_Price(self, B_G_ICPF, B_G_ROW,B_G_CB, b_G):
+        return (B_G_ICPF + B_G_ROW + B_G_CB)/b_G
+    
+    def NetLending_G(self, Tx_H, Tx_F, G, T_H, T_F, i_BG, B_G_ICPF, B_G_ROW):
+        return Tx_H + Tx_F - G - T_H - T_F - i_BG*(B_G_ICPF + B_G_ROW)
+    
+class Bank:
+    
+    def Bank_Bond_Price(self, B_B_ICPF, B_B_ROW, b_B):
+        return (B_B_ICPF + B_B_ROW) / b_B
+    
+    def Interest_rate_BB(self, i_BG):
+        return rho_B_1 + i_BG + rho_B_2 * Bank.Bank_Bond_Price(self,B_B_ICPF, B_B_ROW, b_B)
+    
