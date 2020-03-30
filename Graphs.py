f7087b5a3667f6dbd86658a1d0dc4076bc2cab70
Rahima-web <59868417+Rahima-web@users.noreply.github.com>
2020-03-30 13:01:00 +0200

    Create Graphs.py

---
+++ Graphs.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 11:39:04 2020
+
+@author: sagan
+"""
+from data_preparation import final_dataframe
+
+#Visualisation des données
+tmp = final_dataframe.reset_index()
+tmp = tmp[tmp["Date"] >= "1960"] # Données après 1960
+tmp["Date"] = tmp["Date"].astype(str)
+
+for col in final_dataframe.columns:
+    tmp.plot(x='Date', y=col, marker='.', figsize=(15,7))
+
