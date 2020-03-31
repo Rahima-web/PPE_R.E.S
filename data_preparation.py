75107b82988c69575cded53e0047d3e34914dc96
sagana <sagana1@live.fr>
2020-03-31 15:46:24 +0200

    A table with train and test
    A table with train and test

--- data_preparation.py
+++ data_preparation.py
@@ -151,16 +151,20 @@ dfs = [df.set_index('Date') for df in dfs]
 #Notre dataframe train s'appelle f_train
 final_dataframe = pd.concat([df for df in dfs], join='outer', axis=1)
 final = final_dataframe[1440:1668]
-final_train = final[:132]
-print(final_train)
+final_train = final[:143]
+
 final_train.to_csv("result_train.csv")
 f_train = pd.read_csv("result_train.csv", index_col = "Date")
 f_train.fillna((f_train.mean()), inplace=True)
 #f_train.to_csv("r_train.csv")   #(permet de voir le dataframe f_train complété au max)
 
 #Notre dataframe test s'appelle f_test
-final_test = final[132:]
+final_test = final[143:]
 final_test.to_csv("result_test.csv")
 f_test = pd.read_csv("result_test.csv", index_col = "Date")
 f_test.fillna((f_test.mean()), inplace=True)
 #f_test.to_csv("r_test.csv")    #(permet de voir le dataframe f_test complété au max)
+
+tab = [f_train, f_test]
+tab = pd.concat(tab)
+print(tab)
